1b389653d6bbc4ba93f6da34f4934051a77a00b4
Isha Shinde <ishashinde2021@gmail.com>
2024-08-13 15:12:53 +0530

    Added: daily Function

--- Employee.java
+++ Employee.java
@@ -1,17 +1,34 @@
 import java.util.*;
 class EmployeeWage{
+
+    private int wagePerHr=20;
+    private int FullDayHr=8;
+    private int totalWage=0;
     
+    public void dailywage(boolean add){
+    
+        if(add==true){
+
+            totalWage=totalWage+(wagePerHr *FullDayHr);
+        }else{
+
+            totalWage=totalWage+0;
+        }
+        System.out.println("TotalWage= "+totalWage);
+    }
     public void isPresent(){
     Random random = new Random();       
     // Generate a random number (0 or 1)
     int attendance = random.nextInt(2);
     
     // Check if the employee is present or absent
-        if (attendance == 0) {
-          System.out.println("Employee is absent.");
-         } else {
-          System.out.println("Employee is present.");
-      }
+        if (attendance == 1) {
+             dailywage(true);
+        }else{
+
+            dailywage(false);
+        }
+        
     }
     public static void main(String[] args){
 
