165a2047127057c32a652f317e43cde3c1064f2c
Isha Shinde <ishashinde2021@gmail.com>
2024-08-13 15:07:55 +0530

    Added Present Function

--- Employee.java
+++ Employee.java
@@ -1,9 +1,23 @@
 import java.util.*;
+class EmployeeWage{
+    
+    public void isPresent(){
+    Random random = new Random();       
+    // Generate a random number (0 or 1)
+    int attendance = random.nextInt(2);
+    
+    // Check if the employee is present or absent
+        if (attendance == 0) {
+          System.out.println("Employee is absent.");
+         } else {
+          System.out.println("Employee is present.");
+      }
+    }
+    public static void main(String[] args){
 
-class Employee
-{
-    public static void main(String arg[])
-    {
-        System.out.println("Welcome to Employee");
+        System.out.println("Welcome to Employee wage computation program");
+
+        EmployeeWage ew1=new EmployeeWage();
+        ew1.isPresent();
     }
 }
